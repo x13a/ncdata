f28d3444572323a3908ccdf11223ad7d4b94f109
lucky <>
2020-05-13 18:01:47 +0300

    removed outdated test

--- ncprivacy/test_ncprivacy.py
+++ ncprivacy/test_ncprivacy.py
@@ -27,11 +27,9 @@ class NCPrivacyTestCase(unittest.TestCase):
 
     def test_records(self):
         cur = self.cur
-        cpr1 = ncprivacy.count_privacy_records(cur, include=['_'])
-        self.assertEqual(cpr1, 0)
-        cpr2 = ncprivacy.count_privacy_records(cur)
-        self.assertLessEqual(len(tuple(ncprivacy.iter_records(cur))), cpr2)
-        self.assertEqual(cpr2, ncprivacy.rm_privacy_records(cur))
+        cpr = ncprivacy.count_privacy_records(cur)
+        self.assertLessEqual(len(tuple(ncprivacy.iter_records(cur))), cpr)
+        self.assertEqual(cpr, ncprivacy.rm_privacy_records(cur))
 
 
 if __name__ == '__main__':
